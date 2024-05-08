d6065e374297a6da9d3a7bed0d9007841106395b
Abu Naim <abunaim@Abus-Air-2.lan>
2024-05-09 03:00:07 +0600

    added sslcommerz

--- js/courseBuyNow.js
+++ js/courseBuyNow.js
@@ -1,26 +1,49 @@
+const urlParams = new URLSearchParams(window.location.search);
+const user_id = localStorage.getItem("user_id");
+const course_id = urlParams.get("id");
+let price = 0;
+
 const buyNow = () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const productId = urlParams.get("id");
-    const checkout = document.getElementById("checkout");
-    const div = document.createElement("div");
-    div.style.display = "flex";
-    div.style.columnGap = "100px";
-    div.style.justifyContent = "center";
-    fetch(`https://quiz-zone-backend.onrender.com/course/list/${productId}/`)
-      .then((res) => res.json())
-      .then((data) => {
-        div.innerHTML = `
+  const checkout = document.getElementById("checkout");
+  const div = document.createElement("div");
+  div.style.display = "flex";
+  div.style.columnGap = "100px";
+  div.style.justifyContent = "center";
+  fetch(`https://quiz-zone-backend.onrender.com/course/list/${course_id}/`)
+    .then((res) => res.json())
+    .then((data) => {
+      price = data.price;
+      div.innerHTML = `
           <img class="rounded" src="${data.image}" alt="">
           <div>
               <h2>${data.name}</h2>
               <p>Course Details: ${data.details}</p>
               <p>Price: ${data.price}</p>
-              <button>Pay</button>
+              <button onclick="pay()">Pay</button>
           </div>
           `;
-        checkout.appendChild(div);
-      });
-  };
-  
-  buyNow();
-  
+      checkout.appendChild(div);
+    });
+};
+
+buyNow();
+
+const pay = () => {
+    console.log("hi");
+    const info = {
+        user_id,
+        course_id,
+        price,
+    };
+    fetch("https://quiz-zone-backend.onrender.com/payment/pay/", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(info),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        window.location.href = data.GatewayPageURL; // Redirect to SSLCommerz payment page
+    });
+};
+
